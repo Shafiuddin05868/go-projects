28bd031d754e2ce57bf3431de8965ea8d4acda67
Shafiuddin Chowdury <shafiuddin05868@gmail.com>
2025-06-22 19:29:28 +0600

    impliment health api and take port input fromflag

--- femProject/main.go
+++ femProject/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"time"
 
+	"github.com/shafiuddin05868/go-projects/femProject/internal/app"
+)
+
+func main() {
+	var port int
+	flag.IntVar(&port, "port", 4040, "port to listen ton ")
+	flag.Parse()
+	app, err := app.NewApplication()
+	if err != nil {
+		panic("failed to create application" + err.Error())
+	}
+	app.Logger.Printf("Server is running at port, %d", port)
+
+	http.HandleFunc("/health", HealthCheck)
+
+	server := &http.Server {
+		Addr: ":3055",
+		IdleTimeout: time.Minute,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+	if err := server.ListenAndServe(); err != nil {
+		app.Logger.Fatal("failed to start server: " + err.Error())
+	}
+}
+
+
+func HealthCheck (w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status is available\n")
 }
